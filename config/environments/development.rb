bd01b8dc5f071f75337cfbc29d55faa433dfdfe2
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-10-27 01:42:55 -0700

    Allow better errors in my host machine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Better errors setup for virtualbox
+  BetterErrors::Middleware.allow_ip! "10.0.2.2"
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
