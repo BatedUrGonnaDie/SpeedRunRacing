d3684f4cca1cf128a091b9793ecf3f0671a63b7a
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-04 05:20:44 -0700

    Add another scope and method
    Removes job call, need to rethink how this will happen

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,14 +1,17 @@
 class Race < ApplicationRecord
   has_many :entrants
-  belongs_to :category, required: true
+  belongs_to :category
   has_one :game, through: :category
-
-  after_commit { RaceRelayJob.perform_later(self) }
 
   OPEN = 'Open Entry'.freeze
   PROGRESS = 'In Progress'.freeze
   ENDED = 'Ended'.freeze
   ACTIVE_RACES = [Race::OPEN, Race::PROGRESS].freeze
 
-  scope :active, -> { includes(:game, :category).where(status_text: Race::ACTIVE_RACES) }
+  scope :active, -> { includes(:game, :category, :entrants).where(status_text: Race::ACTIVE_RACES) }
+  scope :completed, -> { includes(:game, :category, :entrants).where(status_text: Race::ENDED) }
+
+  def started?
+    start_time.present?
+  end
 end
