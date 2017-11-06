d56dd113fddb55734bb22d39fbbd6b289569e675
batedurgonnadie <batedurgonnadie@yahoo.com>
2017-11-06 01:27:21 -0800

    Fix emails not sending in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,13 +58,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { :host => ENV['MAILER_HOST'] }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    port: '587',
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
