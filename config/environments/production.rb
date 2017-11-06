405b71eafb73bd3b591ea62b9ce28155b44376d1
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-05 22:50:08 -0800

    one more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_HOST'] }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
