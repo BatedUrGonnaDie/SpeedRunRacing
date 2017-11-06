030128534ee921c0d02d5f9d41f6e628b0bd9919
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-05 19:08:25 -0800

    Add seeds for game/category
    Small update to the readme as well

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ to appliction.yml.  Run any pending migrations and you are good to go.
 
 # Goals
 
-1. Full user login, with the ability to link twitch accounts
+1. ~~Full user login, with the ability to link twitch accounts~~ Completed
 2. Race coordinator
 3. Websocket server that connects users to coordinator to allow native
 in browser racing
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+games = Game.create([
+  {name: 'Super Mario Sunshine', shortname: 'sms',
+    weblink: 'https://www.speedrun.com/sms', cover_large: 'https://www.speedrun.com/themes/sms/cover-256.png',
+    cover_small: 'https://www.speedrun.com/themes/sms/cover-32.png'}
+])
+
+Category.create([
+  {name: 'Any%', game: games.first, weblink: 'https://www.speedrun.com/sms#Any%'},
+  {name: '120 Shines', game: games.first, weblink: 'https://www.speedrun.com/sms#120_Shines'}
+])
