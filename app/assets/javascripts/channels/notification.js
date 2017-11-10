75b8d7a1b110e56771ad9193b9237c416d2ccb91
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-09 18:11:06 -0800

    Fix all the buttons and use real table cells
    Other dom elements still need to be manipulated off the racing events, as well as off the users events (add to entrants table, update readines, etc)

--- app/assets/javascripts/channels/notification.js
+++ app/assets/javascripts/channels/notification.js
@@ -18,14 +18,14 @@ App.notification = App.cable.subscriptions.create("NotificationChannel", {
       case "race_entry_success":
         $(".btn-join-race").addClass("hidden").removeClass("show");
         $(".btn-part-race").addClass("show").removeClass("hidden");
-        $(".btn-race-ready").removeClass("disabled");
+        $(".btn-ready-race").prop("disabled", false);
         break;
       case "race_entry_removed":
         $(".btn-join-race").addClass("show").removeClass("hidden");
         $(".btn-part-race").addClass("hidden").removeClass("show");
         $(".btn-ready-race").addClass("show").removeClass("hidden");
         $(".btn-unready-race").addClass("hidden").removeClass("show");
-        $(".btn-ready-race").addClass("disabled");
+        $(".btn-ready-race").prop("disabled", true);
         break;
       case "race_entry_ready":
         $(".btn-ready-race").addClass("hidden").removeClass("show");
