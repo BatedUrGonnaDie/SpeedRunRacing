92664e8a5e71dec0b6ad0b34f14e565ece93c079
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-16 14:55:59 -0800

    Add proper time formatter

--- app/assets/javascripts/time_formatter.js
+++ app/assets/javascripts/time_formatter.js
@@ -1,3 +1,26 @@
 $(document).on("turbolinks:load", function() {
-
+  $(".format-time").each(function() {
+    var time = $(this).html();
+    if (time === "-" || isNaN(time))
+      return;
+    var new_time = format_time(time);
+    $(this).html(new_time);
+  });
 });
+
+var format_time = function(time_string) {
+  if (time_string === '-')
+    return;
+  var seconds = time_string % 60;
+  var minutes = ~~(time_string / 60);
+  var hours = ~~(minutes / 60);
+  var formatted_time = "" + seconds;
+  if (seconds < 10)
+    formatted_time = "0" + formatted_time;
+  formatted_time = minutes + ":" + formatted_time;
+  if (minutes < 10 && hours === 0)
+    formatted_time = "0" + formatted_time;
+  if (hours > 0)
+    formatted_time = hours + ":" + formatted_time;
+  return formatted_time;
+}
