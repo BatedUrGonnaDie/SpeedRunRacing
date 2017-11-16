5497503e7088654f464136549c94fb6f47180df3
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-16 15:45:57 -0800

    Style slightly differently
    Add race status text and time template to race page

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,2 +1,13 @@
 module RacesHelper
+  def race_color(race)
+    if race.status_text == Race::OPEN
+      'text-success'
+    elsif race.status_text == Race::PROGRESS
+      'text-warning'
+    elsif race.status_text == Race::ENDED
+      'text-danger'
+    else
+      raise
+    end
+  end
 end
