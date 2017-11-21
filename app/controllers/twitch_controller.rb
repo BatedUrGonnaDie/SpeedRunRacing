c3d3098c4e574e1253a64f6cda48c5e6b66b65d3
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-21 14:43:29 -0800

    refactor to fix rubocop errors

--- app/controllers/twitch_controller.rb
+++ app/controllers/twitch_controller.rb
@@ -4,22 +4,26 @@ class TwitchController < ApplicationController
   def create
     auth = request.env['omniauth.auth']
 
+    if update_user(auth)
+      redirect_to root_path, notice: "Successfully linked Twitch account: #{auth.info.nickname}!"
+    else
+      redirect_to root_path, alert: "Error: #{user.errors.full_messages.join(', ')}."
+    end
+  end
+
+  def failure
+    redirect_to redirect_path, alert: "Error: #{params[:message]}"
+  end
+
+  private
+
+  def update_user(auth)
     user = current_user
+
     user.update(
       twitch_name: auth.info.nickname,
       twitch_display_name: auth.info.name,
       twitch_id: auth.uid
     )
-
-    if user.errors.present?
-      redirect_to root_path, alert: "Error: #{user.errors.full_messages.join(', ')}."
-      return
-    end
-
-    redirect_to root_path, notice: "Successfully linked Twitch account: #{auth.info.nickname}!"
-  end
-
-  def failure
-    redirect_to redirect_path, alert: "Error: #{params[:message]}"
   end
 end
