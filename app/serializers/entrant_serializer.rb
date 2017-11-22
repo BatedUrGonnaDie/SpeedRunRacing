54a551f57d8072702762364d9a1a095de381cb6f
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-21 19:26:19 -0800

    add duration to the entrant serializer

--- app/serializers/entrant_serializer.rb
+++ app/serializers/entrant_serializer.rb
@@ -1,5 +1,9 @@
 class EntrantSerializer < ActiveModel::Serializer
-  attributes :id, :ready, :finish_time, :place, :created_at, :updated_at
+  attributes :id, :ready, :finish_time, :place, :duration, :created_at, :updated_at
 
   belongs_to :user
+
+  def duration
+    object.duration
+  end
 end
