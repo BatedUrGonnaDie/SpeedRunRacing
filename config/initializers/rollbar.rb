fa8a62296d2c9cb8537c5f8337881a37df3f9e18
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-11-26 15:58:04 -0800

    remove js stuff for now

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,14 +1,7 @@
+require 'rollbar'
 Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   config.enabled = (Rails.env == 'production')
-
-  config.js_enabled = true
-  config.js_options = {
-    accessToken: "85e887cd33764f4fb70bb23a29be4191",
-    captureUncaught: true,
-    payload: {
-      environment: "production"
-    }
-  }
+  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
 end
