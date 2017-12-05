bab370a126632e9ced9af10ae028cb04054438e9
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-12-05 15:19:49 -0800

    wrong location object

--- app/assets/javascripts/channels/main.js
+++ app/assets/javascripts/channels/main.js
@@ -27,7 +27,7 @@ $(document).on("turbolinks:load", function() {
             if ($("#active-race-table").length) {
               $(selector).prepend(data.html);
             } else {
-              if (window.location.toString() == "/")
+              if (window.location.pathname == "/")
                 Turbolinks.visit(window.location.toString(), { action: 'replace' });
             }
             break;
