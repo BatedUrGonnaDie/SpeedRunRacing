6833e4d5aca4a9f203283f9502a07eb2011f3a2a
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-07-09 16:52:28 -0700

    fix not found error
    change ams adapter and include user in entrants for ams

--- app/controllers/api/v1/races_controller.rb
+++ app/controllers/api/v1/races_controller.rb
@@ -7,24 +7,31 @@ class Api::V1::RacesController < Api::V1::ApplicationController
     case race_types
     when 'completed'
       @races = Race.completed
-      paginate json: @races, serializer: Api::V1::RaceSerializer
+      paginate json: @races,
+               serializer: Api::V1::RaceSerializer,
+               adapter: :json,
+               include: ['entrants', 'entrants.user', 'game', 'category']
     when 'active'
       @races = Race.active
-      render json: @races, serializer: Api::V1::RaceSerializer
+      render json: @races,
+             serializer: Api::V1::RaceSerializer,
+             adapter: :json,
+             include: ['entrants', 'entrants.user', 'game', 'category']
     end
   end
 
   def show
-    render json: @race, serializer: Api::V1::RaceSerializer
+    render json: @race,
+           serializer: Api::V1::RaceSerializer,
+           adapter: :json,
+           include: ['entrants', 'entrants.user', 'game', 'category']
   end
 
   private
 
   def set_race
-    @race = Race.find(params[:race_id])
-
-    return if @race.present?
-
+    @race = Race.includes(:game, :category, :entrants).find(params[:race_id])
+  rescue ActiveRecord::RecordNotFound
     not_found('race', :race_id)
   end
 end
