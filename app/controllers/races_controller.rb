659043c50fe3ca108204e7916659eda052f8cef2
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-08-12 13:39:26 -0700

    fix AR not found error not being caught properly

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,24 +1,27 @@
 class RacesController < ApplicationController
+  before_action :set_race, only: [:show]
+
   def index
     @races = Race.includes(:game, :category).active.order(status_text: :desc)
   end
 
   def show
-    @race = Race.includes(:game, :category, :entrants, :users).find(params[:id])
-
-    if @race.present?
-      @chat_room = @race.chat_room
-      @chat_messages = @race.chat_messages.includes(:user)
-      gon.race = @race
-      gon.chat_room = @race.chat_room
-    else
-      @error = {resource: 'race', name: params[:id]}
-      render 'shared/not_found'
-      return
-    end
+    @chat_room = @race.chat_room
+    @chat_messages = @race.chat_messages.includes(:user)
+    gon.race = @race
+    gon.chat_room = @race.chat_room
   end
 
   def completed
     @races = Race.includes(:game, :category).completed.order(finish_time: :desc).page(params[:page])
+  end
+
+  private
+
+  def set_race
+    @race = Race.includes(:game, :category, :entrants, :users).find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    @error = {resource: 'race', name: params[:id]}
+    render 'shared/not_found'
   end
 end
